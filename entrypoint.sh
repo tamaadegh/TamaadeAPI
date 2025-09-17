05ed365bcfb834a66f89ccdc55fff9a695c34cd9
Bra Koose <developerkoose@gmail.com>
2025-09-17 02:40:56 +0000

    render db issues fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-echo 'Waiting for postgres...'
-
-while ! nc -z $DB_HOSTNAME $DB_PORT; do
-    sleep 0.1
-done
-
-echo 'PostgreSQL started'
+# Check if we're in Render environment (has DATABASE_URL)
+if [ -n "$DATABASE_URL" ]; then
+    echo 'Running in Render environment...'
+    echo 'Database connection will be handled by Django settings'
+else
+    echo 'Waiting for postgres...'
+    while ! nc -z $DB_HOSTNAME $DB_PORT; do
+        sleep 0.1
+    done
+    echo 'PostgreSQL started'
+fi
 
 echo 'Running migrations...'
 python manage.py migrate
