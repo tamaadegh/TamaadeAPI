b8b85abecd1adce5761e864d8a69053c4731f1fd
Bra Koose <developerkoose@gmail.com>
2025-09-17 15:09:18 +0000

    app crashed issues

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 echo "Running database migrations..."
-python manage.py migrate
+python manage.py migrate --no-input
 
 echo "Build completed successfully!"
