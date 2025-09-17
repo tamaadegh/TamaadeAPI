b180a1d03171cc17cb878b3028620ff94388b49f
Bra Koose <developerkoose@gmail.com>
2025-09-17 14:08:49 +0000

    DB Hoting

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,10 +1,16 @@
 import os
-
+import dj_database_url
 from .base import *
+
+# Use DATABASE_URL for external database connection
+DATABASES = {
+    'default': dj_database_url.parse(
+        config('DATABASE_URL', default='postgresql://localhost/tamaade')
+    )
+}
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles")
-
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")
